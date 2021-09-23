30b62c6c070e6197c76d63394199b62787bde1ae
Santosh Shetty <santosh.shetty@synacor.com>
2021-09-23 20:00:55 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
-    <h1>Ready!! Awesome.. Let's do something.</h1>,
+    <App />,
     document.getElementById('root')
 );
