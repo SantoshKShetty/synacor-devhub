2a0d99e1cdc2d5e8f141671662d6452fe8bf8af2
Santosh Shetty <santosh.shetty@synacor.com>
2021-10-29 13:56:34 +0530

    User options on header menu

--- src/screens/dashboard/index.js
+++ src/screens/dashboard/index.js
@@ -11,6 +11,13 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Text from '../../components/text';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import HelpOutline from '@material-ui/icons/HelpOutline';
+import Apps from '@material-ui/icons/Apps';
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUp from '@material-ui/icons/KeyboardArrowUp';
 
 const SearchBox = () => (
     <Box type={HORIZONTAL} style={{ alignItems: 'center', padding: '0 10px', height: 36 }}>
@@ -23,6 +30,82 @@ const SearchBox = () => (
     </Box>
 );
 
+const HelpMenu = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <React.Fragment>
+            <IconButton onClick={handleClick}>
+                <HelpOutline />
+            </IconButton>
+            <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose} getContentAnchorEl={null} anchorOrigin={{ vertical: 'bottom', horizontal: 'left'}} transformOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <MenuItem onClick={handleClose}>Help Center</MenuItem>
+                <MenuItem onClick={handleClose}>Developer Documentation</MenuItem>
+                <MenuItem onClick={handleClose}>Training</MenuItem>
+            </Menu>
+        </React.Fragment>
+    );
+}
+
+const AppMenu = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <React.Fragment>
+            <IconButton onClick={handleClick}>
+                <Apps />
+            </IconButton>
+            <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose} getContentAnchorEl={null} anchorOrigin={{ vertical: 'bottom', horizontal: 'left'}} transformOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <MenuItem onClick={handleClose}>End-user dashboard</MenuItem>
+            </Menu>
+        </React.Fragment>
+    );
+}
+
+const UserMenu = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <React.Fragment>
+            <IconButton onClick={handleClick}>
+                <Box style={{ border: '1px solid', borderRadius: '50%', padding: 5, boxSizing: 'border-box', fontSize: '1rem' }}>JD</Box>
+                {anchorEl ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+            </IconButton>
+            <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose} getContentAnchorEl={null} anchorOrigin={{ vertical: 'bottom', horizontal: 'left'}} transformOrigin={{ vertical: 'top', horizontal: 'center' }}>
+                <MenuItem onClick={handleClose}>User Name</MenuItem>
+                <MenuItem onClick={handleClose}>User Email</MenuItem>
+                <MenuItem onClick={handleClose}>hbo.synacor.com(link to End-user dashboard)</MenuItem>
+                <MenuItem onClick={handleClose}>Settings</MenuItem>
+                <MenuItem onClick={handleClose}>Sign out</MenuItem>
+            </Menu>
+        </React.Fragment>
+    );
+}
+
 const Header = ({ logo }) => (
     <Box type={HORIZONTAL} style={{ alignItems: "center", justifyContent: 'space-between', padding: '0 32px' }}>
         <Box>
@@ -31,12 +114,13 @@ const Header = ({ logo }) => (
         <Box style={{ border: '1px solid #D8D8D8', flexGrow: 1, maxWidth: 650 }}>
             <SearchBox />
         </Box>
-        <Box type={HORIZONTAL}></Box>
+        <Box type={HORIZONTAL} style={{ alignItems: "center" }}>
+            <Box><HelpMenu /></Box>
+            <Box><AppMenu /></Box>
+            <Box><UserMenu /></Box>
+        </Box>
     </Box>
 );
-
-
-// const Acc = () => ()
 
 const LeftCol = () => {
     const options = {
@@ -67,7 +151,7 @@ const LeftCol = () => {
         <Box>
             {Object.entries(options).map(([title, contents], key) => {
                 return (
-                    <Accordion key={key}>
+                    <Accordion key={`accordian-${key}`}>
                         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                             <Text>{title}</Text>
                         </AccordionSummary>
