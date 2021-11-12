646fb320d47a8fe46226ff3967b130e2410e85cc
Santosh Shetty <santosh.shetty@synacor.com>
2021-11-12 17:10:51 +0530

    Table Sorter and Filter Menu added

--- src/screens/dashboard/users.js
+++ src/screens/dashboard/users.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import Box, { HORIZONTAL } from '../../components/box';
-import { TableContainer, Table, TableBody, TableRow, TableCell, TableHead, IconButton } from '@material-ui/core';
+import { TableContainer, Table, TableBody, TableRow, TableCell, TableHead, IconButton, TableSortLabel } from '@material-ui/core';
 import Text from '../../components/text';
 import Button from '../../components/button';
 import FilterList from '@material-ui/icons/FilterList';
@@ -28,22 +30,90 @@ const DATA = [
     createData('Aston Martin', 'Martin', 'amart@xyz.com', 'Active')
 ];
 
+const FilterMenu = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <React.Fragment>
+            <IconButton onClick={handleClick}>
+                <FilterList />
+            </IconButton>
+            <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose} getContentAnchorEl={null} anchorOrigin={{ vertical: 'bottom', horizontal: 'left'}} transformOrigin={{ vertical: 'top', horizontal: 'left' }}>
+                <MenuItem onClick={handleClose}>Display Quick Filter</MenuItem>
+                <MenuItem onClick={handleClose}>By User Type</MenuItem>
+                <MenuItem onClick={handleClose}>By Status</MenuItem>
+                <MenuItem onClick={handleClose}>By Group</MenuItem>
+                <MenuItem onClick={handleClose}>By Apps</MenuItem>
+            </Menu>
+        </React.Fragment>
+    );
+}
+
+function descendingComparator(a, b, sortBy) {
+    if (b[sortBy] < a[sortBy]) {
+        return -1;
+    }
+    if (b[sortBy] > a[sortBy]) {
+        return 1;
+    }
+
+    return 0;
+}
+
+function getComparator(sortBy, sortOrder) {
+    return sortOrder === 'desc'
+        ? (a, b) => descendingComparator(a, b, sortBy)
+        : (a, b) => -descendingComparator(a, b, sortBy);
+}
+
+function stableSort(comparator) {
+    const stabilizedThis = DATA.map((el, index) => [el, index]);
+
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+
+    return stabilizedThis.map((el) => el[0]);
+}
+
+function getSortedData(sortBy, sortOrder) {
+    return stableSort(getComparator(sortBy, sortOrder));
+}
+
 export default function Users() {
+    const [sortBy, setSortBy] = React.useState('fullName');
+    const [sortOrder, setSortOrder] = React.useState('asc');
+
+    const handleOnSort = sortByField => () => {
+        setSortBy(sortByField);
+        setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')
+    }
+
+    const data = getSortedData(sortBy, sortOrder);
+
     return (
         <Box style={{ width: 824 }}>
             <Box type={HORIZONTAL} style={{ justifyContent: 'space-between', paddingBottom: '1.5rem' }}>
-                <Box type={HORIZONTAL}>
+                <Box type={HORIZONTAL} style={{ alignItems: 'center' }}>
                     <Box>
                         <Text color="secondary">Users</Text>
                         <Text color="secondary" variant="caption">Manage Users</Text>
                     </Box>
-                    <IconButton>
-                        <FilterList color="secondary" />
-                    </IconButton>
+                    <FilterMenu />
                 </Box>
                 <Box type={HORIZONTAL}>
-                    <Button label="Add User" />
-                    <Button label="Import from CSV" />
+                    <Button label="Add User" style={{ paddingLeft: '1rem', paddingRight: '1rem', marginRight: '1rem' }} />
+                    <Button label="Import from CSV" style={{ paddingLeft: '1rem', paddingRight: '1rem' }} />
                 </Box>
             </Box>
             <Box>
@@ -52,18 +122,24 @@ export default function Users() {
                         <TableHead>
                             <TableRow>
                                 <TableCell>
-                                    <Text color="secondary">FULL NAME AND USERNAME</Text>
+                                    <TableSortLabel onClick={handleOnSort('fullName')} active={sortBy === 'fullName'} direction={sortBy === 'fullName' ? sortOrder : 'asc'}>
+                                        <Text color="secondary">FULL NAME AND USERNAME</Text>
+                                    </TableSortLabel>
                                 </TableCell>
                                 <TableCell>
-                                    <Text color="secondary">PRIMARY EMAIL</Text>
+                                    <TableSortLabel onClick={handleOnSort('email')} active={sortBy === 'email'} direction={sortBy === 'email' ? sortOrder : 'asc'}>
+                                        <Text color="secondary">PRIMARY EMAIL</Text>
+                                    </TableSortLabel>
                                 </TableCell>
                                 <TableCell>
-                                    <Text color="secondary">STATUS</Text>
+                                    <TableSortLabel onClick={handleOnSort('status')} active={sortBy === 'status'} direction={sortBy === 'status' ? sortOrder : 'asc'}>
+                                        <Text color="secondary">STATUS</Text>
+                                    </TableSortLabel>
                                 </TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {DATA.map(({ fullName, userName, email, status }, i) => (
+                            {data.map(({ fullName, userName, email, status }, i) => (
                                 <TableRow key={`table-row-${i}`} hover>
                                     <TableCell>
                                         <Box>
