edb8ac912e02d94169d48300be3534e4d2f8de7d
Santosh Shetty <santosh.shetty@synacor.com>
2021-12-29 15:45:50 +0530

    A small refactoring to introduce column selector

--- src/screens/dashboard/sections/users/index.js
+++ src/screens/dashboard/sections/users/index.js
@@ -1,32 +1,81 @@
 import React from 'react';
+import dlv from 'dlv';
 import Box, { HORIZONTAL } from '../../../../components/box';
-import { TableContainer, Table, TableBody, TableRow, TableCell, TableHead, IconButton, TableSortLabel } from '@material-ui/core';
 import Text from '../../../../components/text';
 import Button from '../../../../components/button';
 import { generateComponent } from '../../../../utils/component';
+import { TableContainer, TableHead, TableRow, Table, TableCell, TableSortLabel, TableBody } from '@material-ui/core';
 
-function createData(fullName, userName, email, status) {
-    return { fullName, userName, email, status }
-}
-
-const DATA = [
-    createData('John Doe', 'Joedaa', 'joe@cableco.zimbra.cloud', 'Active'),
-    createData('Robert Lastname', 'Robbiee', 'rob@xyz.com', 'Active'),
-    createData('Joe Kayle', 'Joe', 'jk@xyz.com', 'Inactive'),
-    createData('Aston Martin', 'Martin', 'amart@xyz.com', 'Active'),
-    createData('John Doe', 'Joedaa', 'joe@cableco.zimbra.cloud', 'Active'),
-    createData('Robert Lastname', 'Robbiee', 'rob@xyz.com', 'Active'),
-    createData('Joe Kayle', 'Joe', 'jk@xyz.com', 'Inactive'),
-    createData('Aston Martin', 'Martin', 'amart@xyz.com', 'Supsended'),
-    createData('John Doe', 'Joedaa', 'joe@cableco.zimbra.cloud', 'Active'),
-    createData('Robert Lastname', 'Robbiee', 'rob@xyz.com', 'Active'),
-    createData('Joe Kayle', 'Joe', 'jk@xyz.com', 'Inactive'),
-    createData('Aston Martin', 'Martin', 'amart@xyz.com', 'Active'),
-    createData('John Doe', 'Joedaa', 'joe@cableco.zimbra.cloud', 'Active'),
-    createData('Robert Lastname', 'Robbiee', 'rob@xyz.com', 'Active'),
-    createData('Joe Kayle', 'Joe', 'jk@xyz.com', 'Inactive'),
-    createData('Aston Martin', 'Martin', 'amart@xyz.com', 'Active')
+// To render header/pick data key for sorting, keep a good sort key name etc...
+const HEADER_FIELD_DATA_MAP = [
+    {
+        label: "FULL NAME",
+        sortKey: "FULL_NAME",
+        dataKey: "fullName"
+    },
+    {
+        label: "FIRST NAME",
+        sortKey: "FIRST_NAME",
+        dataKey: "firstName"
+    },
+    {
+        label: "LAST NAME",
+        sortKey: "LAST_NAME",
+        dataKey: "lastName"
+    },
+    {
+        label: "USER ID",
+        sortKey: "USER_ID",
+        dataKey: "userId"
+    },
+    {
+        label: "EMAIL",
+        sortKey: "CONTACT_EMAIL",
+        dataKey: "contactEmail"
+    },
+    {
+        label: "STATUS",
+        sortKey: "USER_STATUS",
+        dataKey: "userStatus"
+    },
 ];
+
+// Data received from server
+const DATA = [
+    {
+        userId: "willow_test",
+        userStatus: "ACTIVE",
+        fullName: "willow_test"
+    },
+    {
+        contactEmail: "roytestuser1@cableco.com",
+        userId: "roytestuser1",
+        userStatus: "INACTIVE",
+        fullName: "roytestuser1"
+    },
+    {
+        contactEmail: "bobuser20@hughes.net",
+        firstName: "Bob",
+        lastName: "Durgin",
+        userId: "bobuser20",
+        userStatus: "ACTIVE",
+        fullName: "Bob Durgin"
+    },
+    {
+        userId: "tvecableco_3",
+        userStatus: "SUSPENDED",
+        fullName: "tvecableco_3"
+    },
+    {
+        userId: "peacock4_vp",
+        userStatus: "ACTIVE",
+        fullName: "peacock4_vp"
+    }
+];
+
+const DEFAULT_COLUMNS = ['FULL_NAME', 'CONTACT_EMAIL', 'USER_STATUS'];
+
+const getColumnDetails = key => HEADER_FIELD_DATA_MAP.find(f => f.sortKey === key);
 
 function descendingComparator(a, b, sortBy) {
     if (b[sortBy] < a[sortBy]) {
@@ -40,9 +89,11 @@ function descendingComparator(a, b, sortBy) {
 }
 
 function getComparator(sortBy, sortOrder) {
+    const dataSortKey = dlv(HEADER_FIELD_DATA_MAP.find(h => h.sortKey === sortBy), 'dataKey');
+
     return sortOrder === 'desc'
-        ? (a, b) => descendingComparator(a, b, sortBy)
-        : (a, b) => -descendingComparator(a, b, sortBy);
+        ? (a, b) => descendingComparator(a, b, dataSortKey)
+        : (a, b) => -descendingComparator(a, b, dataSortKey);
 }
 
 function stableSort(comparator) {
@@ -62,15 +113,16 @@ function getSortedData(sortBy, sortOrder) {
 }
 
 export default function Users({ info: { filter } = {} }) {
-    const [sortBy, setSortBy] = React.useState('fullName');
-    const [sortOrder, setSortOrder] = React.useState('asc');
+    const [sortBy, setSortBy] = React.useState(null);
+    const [sortOrder, setSortOrder] = React.useState(null);
 
     const handleOnSort = sortByField => () => {
         setSortBy(sortByField);
         setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')
     }
 
-    const data = getSortedData(sortBy, sortOrder);
+    const data = sortBy && sortOrder ? getSortedData(sortBy, sortOrder) : DATA;
+    const columns = [...DEFAULT_COLUMNS];
 
     return (
         <Box style={{ width: '100%' }}>
@@ -94,47 +146,42 @@ export default function Users({ info: { filter } = {} }) {
                 ))}
             </Box>
             <Box>
-                <TableContainer>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>
-                                    <TableSortLabel onClick={handleOnSort('fullName')} active={sortBy === 'fullName'} direction={sortBy === 'fullName' ? sortOrder : 'asc'}>
-                                        <Text color="secondary">FULL NAME AND USERNAME</Text>
-                                    </TableSortLabel>
-                                </TableCell>
-                                <TableCell>
-                                    <TableSortLabel onClick={handleOnSort('email')} active={sortBy === 'email'} direction={sortBy === 'email' ? sortOrder : 'asc'}>
-                                        <Text color="secondary">PRIMARY EMAIL</Text>
-                                    </TableSortLabel>
-                                </TableCell>
-                                <TableCell>
-                                    <TableSortLabel onClick={handleOnSort('status')} active={sortBy === 'status'} direction={sortBy === 'status' ? sortOrder : 'asc'}>
-                                        <Text color="secondary">STATUS</Text>
-                                    </TableSortLabel>
-                                </TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {data.map(({ fullName, userName, email, status }, i) => (
-                                <TableRow key={`table-row-${i}`} hover>
-                                    <TableCell>
-                                        <Box>
-                                            <Text>{fullName}</Text>
-                                            <Text color="secondary" variant="caption">{userName}</Text>
-                                        </Box>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Text>{email}</Text>
-                                    </TableCell>
-                                    <TableCell>
-                                        <Text>{status}</Text>
-                                    </TableCell>
+                {data.length && (
+                    <TableContainer>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    {columns.map((c, i) => {
+                                        const t = getColumnDetails(c);
+
+                                        return (
+                                            <TableCell key={`thead-cell-${i}`}>
+                                                <TableSortLabel onClick={handleOnSort(t.sortKey)} active={sortBy === t.sortKey} direction={sortBy === t.sortKey ? sortOrder : 'asc'}>
+                                                    <Text variant="h6">{t.label}</Text>
+                                                </TableSortLabel>
+                                            </TableCell>
+                                        );
+                                    })}
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                            </TableHead>
+                            <TableBody>
+                                {data.map((d, i) => (
+                                    <TableRow key={`tbody-row-${i}`} hover>
+                                        {columns.map((c, i) => {
+                                            const t = getColumnDetails(c);
+
+                                            return (
+                                                <TableCell key={`tbody-cell-${i}`}>
+                                                    <Text>{d[t.dataKey]}</Text>
+                                                </TableCell>
+                                            )
+                                        })}
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                )}
             </Box>
         </Box>
     )
