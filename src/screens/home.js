968f0bb359aefd03b61e682834e157e46b6c0bce
Santosh Shetty <santosh.shetty@synacor.com>
2022-03-09 17:57:50 +0530

    memoized components

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,22 +1,14 @@
 import React from "react";
-import { generateComponent } from "../utils/component";
 import Form from "../components/form";
 
-const InfoSection = ({ screenInfo = [] }) => generateComponent(screenInfo)
-
-const FormSection = ({ form: { controls } }) => (
-    <Form>
-        {generateComponent(controls)}
-    </Form>
-)
-
-export default function HomeScreen({ info, Layout }) {
+function HomeScreen({ info, Layout }) {
     const { logo, screenInfo, form } = info;
 
     return (
-        <Layout logo={logo}>
-            <InfoSection screenInfo={screenInfo} />
-            <FormSection form={form} />
+        <Layout logo={logo} screenInfo={screenInfo}>
+            <Form form={form} />
         </Layout>
     );
 };
+
+export default React.memo(HomeScreen);
