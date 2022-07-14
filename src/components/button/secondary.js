0ab6fa2fb290da12590e36315a614d149ba9d83a
Santosh Shetty <santosh.shetty@PNQM1004-SASHET.local>
2022-07-14 17:03:47 +0530

    IDMEIAM-165: UI Fixes

--- src/components/button/secondary.js
+++ src/components/button/secondary.js
@@ -3,11 +3,15 @@ import Button from '.';
 import { makeStyles } from '../../provider/theme';
 
 const styles = makeStyles(
-    ({ palette }) => ({
+    ({ palette, spacing, typography }) => ({
         secondaryBtn: {
-            backgroundColor: palette.custom.button.secondaryBtn.bgColor,
+            width: 'fit-content',
+            height: 40,
+            padding: `0 ${spacing(4)}px`,
+            fontWeight: typography.fontWeightBold,
             color: palette.text.secondary,
-            border: `1px solid ${palette.custom.button.secondaryBtn.borderColor}`,
+            borderColor: palette.custom.button.secondaryBtn.borderColor,
+            backgroundColor: palette.custom.button.secondaryBtn.bgColor,
 
             '&:hover': {
                 backgroundColor: palette.custom.button.secondaryBtn.bgColorHover
--- src/components/link/index.js
+++ src/components/link/index.js
@@ -3,10 +3,20 @@ import { Link as RouterLink } from 'react-router-dom';
 import ReactLink from '@material-ui/core/Link';
 import cn from 'classnames';
 import { isActiveLink } from '../../utils/route';
+import { makeStyles } from '../../provider/theme';
+
+const styles = makeStyles(
+    ({ palette }) => ({
+        link: {
+            color: palette.custom.link.default.color
+        }
+    })
+);
 
 const ACTIVE_LINK_CLASSNAME = 'activeLink';
 
 export default function Link({ href, label, className, ...props }) {
+    const classes = styles();
     const routerLinkProps = href && !/^https?:/.test(href) && {
         to: href,
         component: RouterLink
@@ -16,7 +26,7 @@ export default function Link({ href, label, className, ...props }) {
         <ReactLink
             {...props}
             {...routerLinkProps}
-            className={cn(className, isActiveLink(href) && ACTIVE_LINK_CLASSNAME)}
+            className={cn(classes.link, className, isActiveLink(href) && ACTIVE_LINK_CLASSNAME)}
         >
             {label}
         </ReactLink>
